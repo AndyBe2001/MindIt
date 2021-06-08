939a80ee855e17dfa4e7f2f7cc029def343dbe3d
BE RAHARISON Andy <andybe034@gmail.com>
2021-06-08 14:04:09 +0800

    View Task UI Done
    Principal Element Placed

---
+++ app/src/main/java/com/example/mindit/View_task.java
@@ -0,0 +1,14 @@
+package com.example.mindit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_task extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_task);
+    }
+}
