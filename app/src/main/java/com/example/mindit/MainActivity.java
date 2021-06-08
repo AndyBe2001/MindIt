c5a49731e956aa9f681893aae98504f4cf5cc3d6
BE RAHARISON Andy <andybe034@gmail.com>
2021-06-08 14:53:12 +0800

    Implemented First Run Check
    Check first run to open welcome page

--- app/src/main/java/com/example/mindit/MainActivity.java
+++ app/src/main/java/com/example/mindit/MainActivity.java
@@ -2,14 +2,30 @@ package com.example.mindit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences prefs = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        prefs = getSharedPreferences("com.example.mindit",MODE_PRIVATE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(prefs.getBoolean("firstRun",true)){
+            Intent myIntent = new Intent(this,Welcome_Page.class);
+            startActivity(myIntent);
+            prefs.edit().putBoolean("firstRun",false).commit();
+        }
     }
 }
